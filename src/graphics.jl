cf1d83ffedbeba88c9892f5d6d8c49faddc31547
Thomas Getgood <thomas@macroexpanse.com>
2023-08-12 17:25:44 -0400

    Refactored to remove global assignments.
    The config datastructure is still global, but that's easy enough to fix as we
    move forward.

--- src/graphics.jl
+++ src/graphics.jl
@@ -2,33 +2,22 @@ import brow
 import GLFW.GLFW as glfw
 import Vulkan as vk
 import DataStructures as ds
+import DataStructures: getin, hashmap, assoc, into
 
 glfw.Init()
 
-function createwindow()
+function createwindow(config)
     glfw.WindowHint(glfw.CLIENT_API, glfw.NO_API)
     glfw.WindowHint(glfw.RESIZABLE, true)
 
-  w = glfw.CreateWindow(800, 600, "not quite a browser")
-
-  # @async begin
-  #   while !glfw.WindowShouldClose(w)
-  #     glfw.PollEvents()
-  #     sleep(2)
-  #   end
-  # end
-
-  return w
+  glfw.CreateWindow(
+    getin(config, [:window, :height]),
+    getin(config, [:window, :width]),
+    "not quite a browser"
+  )
 end
 
-extensions = vcat(
-  glfw.GetRequiredInstanceExtensions(),
-  ["VK_EXT_debug_utils"]
-)
-
-validationlayers = ["VK_LAYER_KHRONOS_validation"]
-
-function containsall(needles, hay)
+function containsall(needles, hay)::Bool
   return [nothing] == indexin([nothing], indexin(needles, hay))
 end
 
@@ -73,21 +62,19 @@ function debugcb(severity, type, datap::Ptr{dumcd}, userData)
   return false
 end
 
-debuginfo = vk.DebugUtilsMessengerCreateInfoEXT(
-  LogLevel.all,
-  LogType.all,
-  @cfunction(
-    debugcb,
-    Bool,
-    (Cuint, Cuint, Ptr{dumcd}, Ptr{Cvoid}))
-)
-
-function debugmsgr(instance)
-  vk.create_debug_utils_messenger_ext(instance, debuginfo)
+function debugmsgr(config, system)
+  vk.create_debug_utils_messenger_ext(
+    get(system, :instance),
+    get(config, :debuginfo)
+  )
 end
 
 
-function instance(extensions, validations; debuginfo=nothing)
+function instance(config)
+  ic = get(config, :instance)
+  validationlayers = get(ic, :validation)
+  extensions = get(ic, :extensions)
+
   @assert containsall(
     extensions,
     map(
@@ -97,7 +84,7 @@ function instance(extensions, validations; debuginfo=nothing)
   ) "unsupported extensions required."
 
   @assert containsall(
-    validations,
+    validationlayers,
     map(
       x -> x.layer_name,
       vk.unwrap(vk.enumerate_instance_layer_properties())
@@ -112,27 +99,20 @@ function instance(extensions, validations; debuginfo=nothing)
     engine_name="integrated"
   )
 
-  if debuginfo === nothing
+  if ds.containsp(config, :debuginfo)
     return vk.create_instance(
       validationlayers,
       extensions;
-      application_info=appinfo)
+      next=get(config, :debuginfo),
+      application_info=appinfo
+    )
   else
     return vk.create_instance(
       validationlayers,
       extensions;
-      next=debuginfo,
       application_info=appinfo
     )
     end
-end
-
-function devicegraphicsqueue(system)
-  vk.get_device_queue(
-    get(system, :device),
-    vk.find_queue_family(get(system, :physicaldevice), vk.QUEUE_GRAPHICS_BIT),
-    0
-  )
 end
 
 function findgraphicsqueue(device)
@@ -156,12 +136,19 @@ function findpresentationqueue(system, device)
   )
 end
 
-function checkdevice(system, device, queues)
+function checkdevice(config, system)
   # props = vk.get_physical_device_properties(device)
   # features = vk.get_physical_device_features(device)
+  supportedextensions = vk.unwrap(
+    vk.enumerate_device_extension_properties(get(system, :physicaldevice))
+  )
 
-  return get(queues, :graphics) !== nothing &&
-    get(queues, :presentation) !== nothing
+  return getin(system, [:queues, :graphics]) !== nothing &&
+    getin(system, [:queues, :presentation]) !== nothing &&
+    containsall(
+      getin(config, [:device, :extensions]),
+      map(x -> x.extension_name, supportedextensions)
+    )
 end
 
 function findqueues(system, device)
@@ -171,60 +158,91 @@ function findqueues(system, device)
   )
 end
 
-function pdevice(system)
-  devs =
-    filter(
-      x -> checkdevice(system, x[1], x[2]),
-      map(
-        x -> [x, findqueues(system, x)],
-        vk.unwrap(vk.enumerate_physical_devices(get(system, :instance)))
-      )
-    )
+function pdevice(config, system)
+  potential = into(
+    ds.emptyvector,
+    map(x -> merge(system, hashmap(
+      :physicaldevice, x,
+      :queues, findqueues(system, x)
+    ))) ∘
+    filter(x -> checkdevice(config, x)),
+    vk.unwrap(vk.enumerate_physical_devices(get(system, :instance)))
+  )
 
-  if length(devs) == 0
+  @info typeof(potential)
+  if ds.emptyp(potential)
     nothing
   else
-    first(devs)
+    first(potential)
   end
 end
 
-function createdevice(system)
-  (pdev, queues) = pdevice(system)
-
-  qci = vk.DeviceQueueCreateInfo(
-    vk.find_queue_family(pdev, vk.QUEUE_GRAPHICS_BIT),
-    [1.0]
-  )
-  dci = vk.DeviceCreateInfo(
-    [qci],
-    validationlayers,
-    []
-  )
-  dev = vk.unwrap(vk.create_device(pdev, dci))
-
-  return merge(system, ds.hashmap(:physicaldevice, pdev, :device, dev, :queues, queues))
+function getqueue(system, queue)
+  i = get(get(system, :queues), queue)
+  return vk.get_device_queue(get(system, :device), i, 0)
 end
 
-function start(system)
-  inst = vk.unwrap(instance(extensions, validationlayers; debuginfo))
-  debug = vk.unwrap(debugmsgr(inst))
-  w = createwindow()
-  surface = glfw.CreateWindowSurface(inst, w)
+function swapchainsupport(system)
+  dev = get(system, :physicaldevice)
+  surface = get(system, :surface)
 
-  system = merge(
-    system,
-    ds.hashmap(
-      :instance, inst,
-      :debugmsgr, debug,
-      :window, w,
-      :surface, surface
-    ))
+  capabilities = vk.get_physical_device_surface_capabilities_khr(dev, surface)
+  formats = vk.get_physical_device_surface_formats_khr(dev, surface)
+  modes = vk.get_physical_device_surface_present_modes_khr(dev, surface)
+end
 
-  system = createdevice(system)
+function createdevice(config, system)
+  system = pdevice(config, system)
+  queues = get(system, :queues)
+  pdev = get(system, :physicaldevice)
+
+  qs2c = ds.vals(ds.into(ds.emptymap, map(x -> ds.MapEntry(x, x)), ds.vals(queues)))
+  qcis::Base.Vector = map(x -> vk.DeviceQueueCreateInfo(x, [1.0]), qs2c)
+
+  dci = vk.DeviceCreateInfo(
+    qcis,
+    getin(config, [:device, :validation], []),
+    getin(config, [:device, :extensions], [])
+  )
+
+  assoc(system, :device, vk.unwrap(vk.create_device(pdev, dci)))
+end
+
+config = hashmap(
+  :instance, hashmap(
+    :extensions,
+    vcat(glfw.GetRequiredInstanceExtensions(), ["VK_EXT_debug_utils"]),
+    :validation, ["VK_LAYER_KHRONOS_validation"]
+  ),
+  :device, hashmap(
+    :extensions, [vk.VK_KHR_SWAPCHAIN_EXTENSION_NAME],
+    :validation, ["VK_LAYER_KHRONOS_validation"]
+  ),
+  :debuginfo, vk.DebugUtilsMessengerCreateInfoEXT(
+    LogLevel.all,
+    LogType.all,
+    @cfunction(
+      debugcb,
+      Bool,
+      (Cuint, Cuint, Ptr{dumcd}, Ptr{Cvoid}))
+  ),
+  :window, hashmap(:width, 1080, :height, 1920)
+)
+
+function init(config)
+  system = hashmap(:instance, vk.unwrap(instance(config)))
+  system = assoc(system, :debugmsgr, vk.unwrap(debugmsgr(config, system)))
+  system = assoc(system, :window, createwindow(config))
+  system = assoc(system, :surface, glfw.CreateWindowSurface(
+    get(system, :instance),
+    get(system, :window)
+  ))
+
+  system = createdevice(config, system)
   return system
 end
 
-system = start(ds.emptyorderedmap)
+system = init(config)
 
 
 function repl_teardown()
