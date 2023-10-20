5833d3580c306fec3c6733ec3d95e76d180bd43f
Thomas Getgood <thomas@macroexpanse.com>
2023-10-20 10:32:43 -0400

    Notes on missing functionality.

--- modules/DataStructures/src/set.jl
+++ modules/DataStructures/src/set.jl
@@ -1,4 +1,5 @@
-import Base
+# FIXME: This is incorrect and incomplete. Not that it could pass for complete
+# even at a glance...
 
 abstract type Set <: Sequential end
 
@@ -33,6 +34,7 @@ end
 
 count(s::PersistenHashSet) = s.count
 
+
 function conj(s::Set, x)
   for e in s.elements
     if e == x
@@ -44,4 +46,18 @@ function conj(s::Set, x)
   else
     PersistentArraySet(conj(s.elements, x))
   end
+end
+
+# Allow nodes to have between nodelength and nodelength/2 elements.
+# Each node stores highest and lowest stored value.
+# Insert into tree recursively. If a node grows too large, split it and
+# propagate the changes to the parent.
+#
+# So long as we can ensure that no node will ever have less than nodelength/2
+# elements (except the rightmost), then we know the tree is balanced and lookup
+# and insertion are O(logn).
+#
+# Caveat: Identity is derived from the comparison function. This might not
+# always be what you want.
+struct PersistentSortedSet
 end
