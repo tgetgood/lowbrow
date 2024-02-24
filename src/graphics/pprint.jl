55133e1f0b3218f476c07aa3ac8c091b8655f288
Thomas Getgood <thomas@macroexpanse.com>
2024-02-24 13:40:19 -0500

    Extended prettyprinting to vk HighLevelStructs.

---
+++ src/graphics/pprint.jl
@@ -0,0 +1,25 @@
+module pprint
+
+import Vulkan as vk
+import DataStructures: showrecur, indent, showseq
+
+function showrecur(io::IO, depth, s::vk.HighLevelStruct)
+  print(io, string(typeof(s)) * ": {\n")
+  for k in fieldnames(typeof(s))
+    indent(io, depth)
+    showrecur(io, depth, k)
+    print(io, " -> ")
+    showrecur(io, depth + 1, getproperty(s, k))
+    print(io, "\n")
+  end
+  indent(io, depth - 1)
+  print(io, "}")
+end
+
+function showrecur(io::IO, depth, t::Tuple)
+  print(io, string(typeof(t)) * ": (\n")
+  showseq(io, depth, t)
+  print(io, ")")
+end
+
+end
